699d90482651e1db3494dd5e3d1f8771cb571c4f
Josh Wilson <joshua.wilson@vanderbilt.edu>
2020-03-05 21:43:38 -0500

    Minor work on cli.

--- packages/cli/src/index.js
+++ packages/cli/src/index.js
@@ -5,23 +5,16 @@ const express = require("express");
 const esm = require("esm");
 const esmRequire = esm(module);
 
-// find all of the node_helper.js files in the modules
-function resolveApp(...p) {
-  return path.resolve(process.cwd(), ...p);
-}
+
+
 const paths = {
   cwd: resolveApp("."),
   appModules: resolveApp("modules"),
   appModulesDefault: resolveApp("modules", "default"),
-  appConfigJs: resolveApp("config", "config.js"),
-}
+  appConfigJs: process.env.MM_CONFIG_FILE || resolveApp("config", "config.js"),
+};
+const mmPort = process.env.MM_PORT || 8080;
 
-// returns array of absolute paths to child directories within parentDir
-function readChildDirs(parentDir) {
-  return fs.readdirSync(parentDir, { withFileTypes: true })
-    .filter(dirent => dirent.isDirectory())
-    .map(dirent => path.join(parentDir, dirent.name))
-}
 
 // Collect all modules dirs except default/, and all modules within default/
 const moduleDirs = readChildDirs(paths.appModules)
@@ -33,33 +26,43 @@ const config = require(paths.appConfigJs);
 
 
 // Collect all of the node_helper.js paths
-let nodeHelpers = {};
-for (const moduleDir of moduleDirs) {
-  const moduleName = moduleDir.slice(moduleDir.lastIndexOf(path.sep) + 1);
-  console.log(`Requiring module ${moduleDir}.`);
-  const nodeHelperPath = path.join(moduleDir, `node_helper.js`);
-  if (fs.existsSync(nodeHelperPath)) {
-    nodeHelpers[moduleName] = nodeHelperPath;
-  }
-}
+let nodeHelpers = collectNodeHelpers();
 
-// TODO: on requests to start Node Helpers from the client, import nodeHelpers[moduleName] with esmLoader()
+
 
 // Initialize the express app
 const app = express();
+// TODO: on requests to start Node Helpers from the client, import nodeHelpers[moduleName] with esmLoader()
 
 
 
 
 
 
+// find all of the node_helper.js files in the modules
+function resolveApp(...p) {
+  return path.resolve(process.cwd(), ...p);
+}
 
+// returns array of absolute paths to child directories within parentDir
+function readChildDirs(parentDir) {
+  return fs.readdirSync(parentDir, { withFileTypes: true })
+    .filter(dirent => dirent.isDirectory())
+    .map(dirent => path.join(parentDir, dirent.name))
+}
 
-
-
-
-
+// returns object of { module: "node_helper paths" }
+function collectNodeHelpers() {
+  let nodeHelpers = {};
+  for (const moduleDir of moduleDirs) {
+    const moduleName = moduleDir.slice(moduleDir.lastIndexOf(path.sep) + 1);
+    console.debug(`Requiring module ${moduleDir}.`);
+    const nodeHelperPath = path.join(moduleDir, `node_helper.js`);
+    if (fs.existsSync(nodeHelperPath)) {
+      nodeHelpers[moduleName] = nodeHelperPath;
+    }
+  }
+  return nodeHelpers;
+}
 
 function checkDeprecatedConfig() {}
-
-
