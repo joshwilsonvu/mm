572fb5facc42cbf2bbb57a351254d18efdda4230
Josh Wilson <joshua.wilson@vanderbilt.edu>
2020-06-11 17:17:17 -0400

    In @mm/moduleslist convert yaml to json at build time, not runtime.

---
+++ packages/moduleslist/build.js
@@ -0,0 +1,18 @@
+
+const yaml = require("yaml");
+const fs = require("fs-extra");
+const path = require("path");
+
+(async function build() {
+  // Parse the YAML file
+  const data = yaml.parse(await fs.readFile(path.resolve(__dirname, "data.yaml"), "utf8"));
+
+  // Convert  { [category]: [{}, {}, {}...] } into a
+  // flat array where each object has a "category" property
+  const list = Object.keys(data).flatMap(category => (
+    data[category].map(entry => ({ ...entry, category }))
+  ));
+
+  // Write the list to lib/index.json
+  await fs.writeJson(path.resolve(__dirname, "lib", "index.json"), list, { spaces: 1 });
+})();
